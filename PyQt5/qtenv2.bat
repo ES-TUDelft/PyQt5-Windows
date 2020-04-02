c1ed5a174ac764ea28077077bbfe42a35390c9dc
Elie <e.saad@tudelft.nl>
2020-04-02 12:50:39 +0200

    Added missing plugins

---
+++ PyQt5/qtenv2.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Setting up environment for Qt usage...
+set PATH=C:\Qt\5.13.2\msvc2017\bin;%PATH%
+cd /D C:\Qt\5.13.2\msvc2017
+echo Remember to call vcvarsall.bat to complete environment setup!
